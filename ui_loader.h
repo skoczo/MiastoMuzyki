b9a3b511c61a04ed0bc1af64795a82d150d7f575
Adrian Skoczylas <adrian.sko>
2011-04-14 18:00:55 +0200

    Slider fix

--- ui_loader.h
+++ ui_loader.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'loader.ui'
 **
-** Created: Mon Mar 28 17:43:43 2011
+** Created: Thu Apr 14 17:27:36 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
